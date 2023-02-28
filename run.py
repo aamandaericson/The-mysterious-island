42ecbf82496fdc0bac2dd6e7f519d7969a9a5f60
aamandaericson <amandaericson1@hotmail.com>
2023-02-28 14:33:32 +0000

    Add print statements to intro function.

--- run.py
+++ run.py
@@ -4,4 +4,10 @@ def intro():
     If input is "y" the game starts and if input is "n" the game quits.
     Any other input leads to demand for input from user again.
     """
-    
+    print("You wake up and open your eyes but you can't see anything")
+    print("You stretch your arms up to your head and feel something weird.")
+    print("A helmet of some sort is on your head.")
+    print("You try to remove it but you can't.")
+    print("You try to pull it of with all of your strength but it's stuck.\n")
+    print('"... What is this... Where am I... What is going on?"\n')
+    print('"... I do not remember anything... Not even my name..."\n')
