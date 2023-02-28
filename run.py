bec820f3c48f6184568d284eb5376fcc751803e2
aamandaericson <amandaericson1@hotmail.com>
2023-02-28 15:52:34 +0000

    Add box_or_search function.

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ def intro():
     answer = str.lower((input("Do you want to start to feel your way around? (y/n): ")))
 
     if answer == "y":
-        choices()
+        box_or_search()
     elif answer == "n":
         print("\nYou give up and accept your fate.")
         print("Days go by and you finally close your eyes and never wake up.")
@@ -27,8 +27,32 @@ def intro():
         intro()
 
 
+def box_or_search():
+    """
+    Function that leads user to different places depending on input.
+    Demands user input. While loop that runs until user input is "y" or "n".
+    In any other case the user will get a message that tells them to input "y" or "n".
+    """
+    print("\nYou realize you will not get the helmet of.")
+    print("You start using your hands to feel around.")
+    print("Crawling around on your knees, you find what feels like a box.\n")
+    while True:
+        answer = input("Do you open the box? (y/n): ")
+        if answer == "y":
+            print("open_box")
+            break
+        elif answer == "n":
+            print("search")
+            break
+        else:
+            print("Incorrect input! Please answer y/n")
+
+
 def main():
     """
     Main fuction to run the other functions.
     """
     intro()
+
+
+main()
