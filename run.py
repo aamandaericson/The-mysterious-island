4bd14d8a06e9576376524e922a0861ed97d53679
aamandaericson <amandaericson1@hotmail.com>
2023-02-28 15:54:33 +0000

    Remove unnecessary whitespace.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 def intro():
     """
-    Function for intro of the game. Input is required by the user. 
+    Function for intro of the game. Input is required by the user.
     If input is "y" the game starts and if input is "n" the game quits.
     Any other input leads to demand for input from user again.
     """
